0a605522e2f12a0da4b15da48c1031de81249a37
Ben Van Treese <vantreeseba@gmail.com>
2013-04-13 23:59:35 -0500

    working on cleaning up conversion and some ideas

--- src/steering.js
+++ src/steering.js
@@ -1,35 +1,6 @@
 var DE = DE || {};
 DE.Steer = function(){
-	var checkConversion = function(entity){		
-		if(entity.pos == undefined){
-			throw new DE.Errors.ConversionError({property: "entity.pos", message: " is undefined. You must set the position vector in the converter."});
-		}
-		if(entity.heading == undefined){
-			throw new DE.Errors.ConversionError({property: "entity.heading", message:" is undefined. You must set the heading vector in the converter."});
-		}
-		if(entity.max_speed == undefined){
-			throw new DE.Errors.ConversionError({property: "entity.max_speed ", message:"is undefined. You must set the max_speed scalar in the converter."});
-		}		
-	}
-
-	var convertAndCheck= function(entity){
-		if(this.converter == undefined || entity instanceof DE.Vector){
-			return entity;
-		}
-		var converted = this.converter(entity);
-		checkConversion(converted);
-		return converted;
-	}
-
-	function Steering(){
-		this.converter = undefined;		
-	};
-
-	Steering.prototype.RegisterEntityConverter = function(converterCallback) {
-		if(converterCallback.length != 1){
-			throw new DE.Errors.ConversionError({message: "The converter callback must take one and only one arguement, the entity to convert."});
-		}
-		this.converter = converterCallback;		
+	function Steering(){		
 	};
 
 	Steering.prototype.Align = function(first_argument) {
@@ -61,7 +32,7 @@ DE.Steer = function(){
 			centerOfMass.Scale(1/neighborCount);
 			return this.Arrive(pos,centerOfMass,max_speed);			
 		}
-
+		
 		return DE.Vec2d(0,0);
 	};
 
